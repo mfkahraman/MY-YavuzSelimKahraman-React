2ce5a1767b438db47f678857be4ffb44660729a3
mfkahraman <mfurkankahraman@gmail.com>
2026-09-11 00:47:28 +0300

    init

---
+++ finance-front/src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Finance App</h1>
+      <p>Hello World</p>
+    </div>
+  );
+}
